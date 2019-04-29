bd8959a97d58e3203b82a97ee226debf18f7c968
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2019-04-29 23:26:50 +0300

    v0.4

---
+++ TextBook_Biology/TextBook_Biology/bin/Debug/Учебник по Биологии 6 класс/js/common.js
@@ -0,0 +1,470 @@
+$(document).ready(function () {
+  
+  $('#test-1').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-3':
+            result += 1;
+            break;
+          case 'answer-5':
+            result += 1;
+            break;
+          case 'answer-11':
+            result += 1;
+            break;
+          case 'answer-13':
+            result += 1;
+            break;
+          case 'answer-20':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-2').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-1':
+            result += 1;
+            break;
+          case 'answer-8':
+            result += 1;
+            break;
+          case 'answer-12':
+            result += 1;
+            break;
+          case 'answer-14':
+            result += 1;
+            break;
+          case 'answer-17':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-3').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-1':
+            result += 1;
+            break;
+          case 'answer-5':
+            result += 1;
+            break;
+          case 'answer-9':
+            result += 1;
+            break;
+          case 'answer-14':
+            result += 1;
+            break;
+          case 'answer-17':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-4').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-2':
+            result += 1;
+            break;
+          case 'answer-7':
+            result += 1;
+            break;
+          case 'answer-10':
+            result += 1;
+            break;
+          case 'answer-16':
+            result += 1;
+            break;
+          case 'answer-18':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-5').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-3':
+            result += 1;
+            break;
+          case 'answer-5':
+            result += 1;
+            break;
+          case 'answer-10':
+            result += 1;
+            break;
+          case 'answer-15':
+            result += 1;
+            break;
+          case 'answer-17':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-6').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-2':
+            result += 1;
+            break;
+          case 'answer-5':
+            result += 1;
+            break;
+          case 'answer-11':
+            result += 1;
+            break;
+          case 'answer-14':
+            result += 1;
+            break;
+          case 'answer-18':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-7').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-3':
+            result += 1;
+            break;
+          case 'answer-5':
+            result += 1;
+            break;
+          case 'answer-9':
+            result += 1;
+            break;
+          case 'answer-13':
+            result += 1;
+            break;
+          case 'answer-20':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-8').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-3':
+            result += 1;
+            break;
+          case 'answer-5':
+            result += 1;
+            break;
+          case 'answer-10':
+            result += 1;
+            break;
+          case 'answer-14':
+            result += 1;
+            break;
+          case 'answer-17':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-9').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-3':
+            result += 1;
+            break;
+          case 'answer-8':
+            result += 1;
+            break;
+          case 'answer-12':
+            result += 1;
+            break;
+          case 'answer-13':
+            result += 1;
+            break;
+          case 'answer-18':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-10').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-1':
+            result += 1;
+            break;
+          case 'answer-7':
+            result += 1;
+            break;
+          case 'answer-11':
+            result += 1;
+            break;
+          case 'answer-15':
+            result += 1;
+            break;
+          case 'answer-18':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-11').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-4':
+            result += 1;
+            break;
+          case 'answer-8':
+            result += 1;
+            break;
+          case 'answer-10':
+            result += 1;
+            break;
+          case 'answer-14':
+            result += 1;
+            break;
+          case 'answer-19':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-12').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-2':
+            result += 1;
+            break;
+          case 'answer-5':
+            result += 1;
+            break;
+          case 'answer-10':
+            result += 1;
+            break;
+          case 'answer-16':
+            result += 1;
+            break;
+          case 'answer-20':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+
+  $('#test-13').click(function () {
+    var result = 0;
+    var inputMas = $("input");
+    for (let i = 0; i < inputMas.length; i++) {
+      const element = inputMas[i];
+      if ($(element).prop("checked")) {
+        switch (element.id) {
+          case 'answer-2':
+            result += 1;
+            break;
+          case 'answer-6':
+            result += 1;
+            break;
+          case 'answer-11':
+            result += 1;
+            break;
+          case 'answer-14':
+            result += 1;
+            break;
+          case 'answer-18':
+            result += 1;
+            break;
+        
+      }
+    }
+  }
+    $("input:radio").prop("checked", false);
+    $(".test-wrap").append(
+      '<div class="question__item">'+
+      '<h3>Количество правильных ответова: '+ result + ' из ' + $("h2").length + '</h3>' +
+      '</div>'
+      );
+      result = 0;
+      $(this).prop("disabled", true).text("Тест завершён");
+  });
+});
